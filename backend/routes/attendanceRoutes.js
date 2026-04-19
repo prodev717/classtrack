4834db10569d6370e73c296aa981ddbced87360a
prodev717 <blackboard717@gmail.com>
2026-04-19 20:55:17 +0530

    flush

--- backend/routes/attendanceRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { logIotAttendance } from "../controllers/attendanceController.js";
-
-const router = express.Router();
-
-router.post("/iot", logIotAttendance);
-
-export default router;
--- frontend/src/components/Badge.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Badge = ({ children, variant = 'success' }) => (
-  <span className={`badge badge-${variant}`}>{children}</span>
-);
-
-export default Badge;
